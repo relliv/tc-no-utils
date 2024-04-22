9f0b33822993ccf63684a3d7047cfceb9f6eb8a3
relliv <epsilonelittr@gmail.com>
2024-04-22 03:51:08 +0300

    Add TS build scripts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { TcNoValidate, ITcNoValidate } from "./utils/tc-no-validate.util"
+import { TcNoGenerate } from "./utils/tc-no-generate.util"
+
+export type { ITcNoValidate }
+export { TcNoGenerate, TcNoValidate }
