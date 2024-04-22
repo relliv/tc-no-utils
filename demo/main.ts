d1a1a09f4c8587ec9280d90ad79e635dae9acfa0
relliv <epsilonelittr@gmail.com>
2024-04-22 06:18:03 +0300

    Update toast configs

--- demo/main.ts
+++ demo/main.ts
@@ -31,12 +31,10 @@ clipboard.on("success", () => {
   Toastify({
     text: "copied to clipboard",
     duration: 3000,
-    destination: "https://github.com/apvarun/toastify-js",
-    newWindow: true,
     close: true,
     gravity: "top", // `top` or `bottom`
     position: "left", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
+    stopOnFocus: false, // Prevents dismissing of toast on hover
     style: {
       background: "linear-gradient(to right, #00b09b, #96c93d)"
     }
